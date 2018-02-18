346b4454b856fabaa8b6be895b093d300c288431
CeaCrap <2902933052@qq.com>
2018-02-18 22:37:47 +0800

    完成

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ var yyy = document.getElementById('xxx');
 //由于画圆划线函数用到了上下文  定义的上下文移到了另一个函数，获取不到，需要放在头部作为全局变量，这样用到上下文的函数都可以获取到了
 var context = yyy.getContext('2d');
 
+var lineWidth = 5
+
 //自动设置canvas宽高
 autoSetCanvasSize(yyy)
 
@@ -15,11 +17,24 @@ pen.onclick = function(){
 	buttonState = false
 	pen.classList.add('active')
 	eraser.classList.remove('active')
+	clear.classList.remove('active')
 }
 eraser.onclick = function(){
 	buttonState = true
 	eraser.classList.add('active')
 	pen.classList.remove('active')
+	clear.classList.remove('active')
+}
+clear.onclick = function(){
+	context.clearRect(0, 0, yyy.width, yyy.height);
+}
+down.onclick = function(){
+	var url = yyy.toDataURL('image/png')
+	var a = document.createElement('a')
+	document.body.appendChild(a)
+	a.href = url
+	a.download = '我画的画'
+	a.click()
 }
 // eraser.onclick = function(){
 //   eraserEnabled = true //橡皮檫使用
@@ -29,21 +44,48 @@ eraser.onclick = function(){
 //   eraserEnabled = false //橡皮檫禁用
 //   actions.className = 'actions'
 // }
+red.onclick = function(){
+	context.fillStyle = 'red'
+	context.strokeStyle = 'red'
+	red.classList.add('active')
+	green.classList.remove('active')
+	blue.classList.remove('active')
+}
+green.onclick = function(){
+	context.fillStyle = 'green'
+	context.strokeStyle = 'green'
+	green.classList.add('active')
+	red.classList.remove('active')
+	blue.classList.remove('active')
+}
+blue.onclick = function(){
+	context.fillStyle = 'blue'
+	context.strokeStyle = 'blue'
+	blue.classList.add('active')
+	green.classList.remove('active')
+	red.classList.remove('active')
+}
 
+thin.onclick = function(){
+	lineWidth = 5
+}
+thick.onclick = function(){
+	lineWidth = 10
+}
 
 //画圆函数
 function huayuan(x,y,radius){ 
   context.beginPath() 
-  context.fillStyle = 'green' //填充圆红色
+  // context.fillStyle = 'green' //填充圆红色
   context.arc(x,y,radius,0,Math.PI*2);
   context.fill()
 }
 //划线函数
 function huaxian(x1,y1,x2,y2){
   context.beginPath();
-  context.strokeStyle = '#666' //线的颜色
+  // context.strokeStyle = '#666' //线的颜色
   context.moveTo(x1,y1)//起点
-  context.lineWidth = 10
+  context.lineWidth = lineWidth
   context.lineTo(x2,y2)//终点
   context.stroke()
   context.closePath()
